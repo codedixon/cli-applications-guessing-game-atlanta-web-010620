95915f47f20c11c60768106fcb38bc24d5759d85
Rashad Dixon <dixoncodes@gmail.com>
2019-12-30 16:32:23 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -8,6 +8,6 @@ def run_guessing_game
   elsif user_input == "exit"
     puts "Goodbye!"
   else
-    puts "/Sorry! the computer guessed #{random_number}./".chomp
+    puts "/Sorry! The computer guessed #{random_number}./".chomp
   end
 end
