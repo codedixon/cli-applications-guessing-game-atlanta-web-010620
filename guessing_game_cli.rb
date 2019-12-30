dc55162adc75f6f65977f2e8952ad83dcd4f0fb8
Rashad Dixon <dixoncodes@gmail.com>
2019-12-30 16:37:24 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,7 +3,7 @@ def run_guessing_game
   random_number = rand(6) + 1
   user_input = gets
   
-  if user_input == random_number
+  if user_input.to_i == random_number
     puts "You guessed the correct number!"
   elsif user_input == "exit"
     puts "Goodbye!"
